a3ade02ee0f5458961094afa264439566f2c2901
Aleksei Anufriev <axoft10@gmail.com>
2022-12-17 12:59:09 +0700

    a new Chess project has been added

--- src/components/grid/GridItem.jsx
+++ src/components/grid/GridItem.jsx
@@ -1,33 +1,29 @@
 import "./Grid.css";
 import parse from "html-react-parser";
 
-const GridItem = ({ item }) => {
+const GridItem = ({
+  item: {
+    link,
+    icon: { path },
+    title,
+    description,
+  },
+}) => {
+  
   return (
-    <div className='card'>
-      <div className='front'>
-        <a
-          href={item.link}
-          rel={"noreferrer"}
-          target="_blank"
-          className='item'
-        >
-          <div
-            className='gradient'
-          > 
-            <img src={item.icon.path} alt="" />
+    <div className="card">
+      <div className="front">
+        <a href={link} rel={"noreferrer"} target="_blank" className="item">
+          <div className="gradient">
+            <img src={require(`../../icons/${path}`)} alt="" />
           </div>
         </a>
       </div>
-      <a
-        href={item.link}
-        rel={"noreferrer"}
-        target="_blank"
-        className='back'
-      >
-        <div className='contentItem'>
-          <h1>{item.title}</h1>
-          <div className='titleItem'></div>
-          <span>{parse(item.description)}</span>
+      <a href={link} rel={"noreferrer"} target="_blank" className="back">
+        <div className="contentItem">
+          <h1>{title}</h1>
+          <div className="titleItem"></div>
+          <span>{parse(description)}</span>
         </div>
       </a>
     </div>
